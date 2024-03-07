fcb5f3f79956ed265d138801ef0b2264ab4e9628
Etienne <34932238+EtienneCClarke@users.noreply.github.com>
2024-03-07 15:21:45 +0000

    Added alert example

--- src/ui/examples/alert-example.tsx
+++ src/ui/examples/alert-example.tsx
@@ -1,4 +1,4 @@
-import { Alert, AlertContent, AlertTitle } from "../components/alert"
+import { Alert, AlertContent, AlertTitle, RocketIcon, SuccessIcon, WarningIcon } from "../components/alert"
 
 export default function AlertExample() {
     return (
@@ -6,27 +6,31 @@ export default function AlertExample() {
             <h1>Alert</h1>
             <div style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
                 <Alert variant="primary">
+                    <RocketIcon />
                     <AlertTitle>Heads up!</AlertTitle>
                     <AlertContent>
                         You can add alerts as components anywhere in your application.
                     </AlertContent>
                 </Alert>
                 <Alert variant="success">
-                    <AlertTitle>Heads up!</AlertTitle>
+                    <SuccessIcon />
+                    <AlertTitle>Success!</AlertTitle>
                     <AlertContent>
-                        You can add alerts as components anywhere in your application.
+                        Check your email for a booking confirmation. We'll see you soon!
                     </AlertContent>
                 </Alert>
                 <Alert variant="warning">
-                    <AlertTitle>Heads up!</AlertTitle>
+                    <WarningIcon />
+                    <AlertTitle>Out of stock!</AlertTitle>
                     <AlertContent>
-                        You can add alerts as components anywhere in your application.
+                        Use the 'warning' variant when you want to notify a user of a non critical issue.
                     </AlertContent>
                 </Alert>
                 <Alert variant="destructive">
-                    <AlertTitle>Heads up!</AlertTitle>
+                    <WarningIcon />
+                    <AlertTitle>Error</AlertTitle>
                     <AlertContent>
-                        You can add alerts as components anywhere in your application.
+                        Your session has expired. Please log in again.
                     </AlertContent>
                 </Alert>
             </div>
