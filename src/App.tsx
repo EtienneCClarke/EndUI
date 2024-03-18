77b1d62bea3a94f3441965ee4abfbafdac6ae52a
Etienne <34932238+EtienneCClarke@users.noreply.github.com>
2024-03-18 16:05:37 +0000

    .

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,21 @@ import AccordionExample from "./ui/examples/accordion-example";
 import AlertExample from "./ui/examples/alert-example";
 import ButtonExample from "./ui/examples/button-example";
 import BadgeExample from "./ui/examples/badge-example";
+import { ToastProvider, useToast } from "./ui/components/toast";
 import "./app.css";
+import ToastExample from "./ui/examples/toast-example";
 
 function App() {
+
+  const toast = useToast();
+
   return (
     <>
       <AccordionExample />
       <AlertExample />
       <BadgeExample />
       <ButtonExample />
+      <ToastExample />
     </>
   );
 }
