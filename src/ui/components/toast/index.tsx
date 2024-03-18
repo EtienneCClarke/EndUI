932366dd8e11947d0f327f2678c9f6c85bde1931
Etienne <34932238+EtienneCClarke@users.noreply.github.com>
2024-03-18 16:04:52 +0000

    Completed toast

--- src/ui/components/toast/index.tsx
+++ src/ui/components/toast/index.tsx
@@ -1,33 +1,33 @@
-import { createContext, useContext, useState, useMemo } from "react";
+import { createContext, useContext, useState, useMemo, useRef, useEffect } from "react";
+import { ToastContextProps, ToastProviderProps, ToastProps, ToastCreateProps } from "./toast.types";
+import css from "./toast.module.css";
+import autoAnimate, { getTransitionSizes } from "@formkit/auto-animate";
 
-type ToastContextValue = {
-    open: (message: string) => void;
-    close: (id: number) => void
-}
-
-type ToastType = {
-    message: string;
-    id: number;
-}
-
-const ToastContext = createContext<ToastContextValue | null>(null);
+const ToastContext = createContext<ToastContextProps>({
+    create: (content: any) => {},
+    close: (id: number) => {}
+});
 
 const useToast = () => useContext(ToastContext);
 
-const ToastProvider = ({
-    children,
-}: {
-    children: React.JSX.Element[] | React.JSX.Element;
-}) => {
+const ToastProvider = ({ children, position = "bottom_right", className }: ToastProviderProps) => {
 
-    const [toasts, setToasts] = useState<ToastType[]>([]);
+    const [toasts, setToasts] = useState<ToastCreateProps[]>([]);
 
-    function openToast(message: string) {
+    const parent = useRef(null);
+    
+    const MAX_TOASTS = 3;
+
+    function createToast({ title, description, action, duration, variant = "primary" }: ToastCreateProps ) {
         const newToast = {
             id: Date.now(),
-            message
+            title,
+            description,
+            action,
+            duration,
+            variant
         };
-        setToasts((prevToasts) => [...prevToasts, newToast])
+        setToasts((prevToasts) => [...prevToasts, newToast]);
     }
 
     function closeToast(id: number) {
@@ -37,33 +37,161 @@ const ToastProvider = ({
     }
 
     const contextValue = useMemo(() => ({
-        open: openToast,
+        create: createToast,
         close: closeToast
     }), [])
 
+    useEffect(() => {
+        parent.current && autoAnimate(parent.current, (el, action, oldCoords, newCoords) => {
+
+            let keyframes;
+    
+            if(action === 'add') {
+                
+                if(position === 'top_left' || position === 'top_right') {
+                    keyframes = [
+                        { transform: 'translateY(calc(-100% - 2rem))', opacity: 0 },
+                        { transform: 'translateY(0)', opacity: 1 }
+                    ]
+                }
+
+                if(position === 'bottom_left' || position === 'bottom_right') {
+    
+                    keyframes = [
+                        { transform: 'translateY(calc(100% + 2rem))', opacity: 0 },
+                        { transform: 'translateY(0)', opacity: 1 }
+                    ]
+    
+                }
+
+            }
+
+            if(action === 'remove') {
+
+                if(position === 'top_right' || position === 'bottom_right') {
+                    keyframes = [
+                        { transform: 'translate(0, 0)', opacity: 1 },
+                        { transform: 'translate(calc(100% + 2rem), 0)', opacity: 0 }
+                    ]
+                }
+
+                if(position === 'top_left' || position === 'bottom_left') {
+                    keyframes = [
+                        { transform: 'translate(0, 0)', opacity: 1 },
+                        { transform: 'translate(calc(-100% - 2rem), 0)', opacity: 0 }
+                    ]
+                }
+
+            }
+
+            if (action === 'remain') {
+                // for items that remain, calculate the delta
+                // from their old position to their new position
+                const deltaX = oldCoords.left - newCoords.left
+                const deltaY = oldCoords.top - newCoords.top
+                // use the getTransitionSizes() helper function to
+                // get the old and new widths of the elements
+                const [widthFrom, widthTo, heightFrom, heightTo] = getTransitionSizes(el, oldCoords, newCoords)
+                // set up our steps with our positioning keyframes
+                const start = { transform: `translate(${deltaX}px, ${deltaY}px)` }
+                const end = { transform: `translate(0, 0)` }
+                // if the dimensions changed, animate them too.
+                if (widthFrom !== widthTo) {
+                  start.width = `${widthFrom}px`
+                  end.width = `${widthTo}px`
+                }
+                if (heightFrom !== heightTo) {
+                  start.height = `${heightFrom}px`
+                  end.height = `${heightTo}px`
+                }
+                keyframes = [start, end]
+            }
+
+            return new KeyframeEffect(el, keyframes, { duration: 300, easing: 'ease-out' });
+    
+        });
+    }, [parent]);
+
     return(
         <ToastContext.Provider value={contextValue}>
-            {children}
-            {toasts &&
-                toasts.map((toast) => {
-                    return (
-                        <Toast
-                            key={toast.id}
-                            message={toast.message}
-                            close={() => closeToast(toast.id)}
-                        />
-                    );
-                })
-            }
+            <>
+                {children}
+                <section
+                    aria-label="Notifications alt-t"
+                    tabIndex={-1}
+                    className={`${css.toasts_container} ${css[position]} ${className ? className : ''}`}
+                    ref={parent}
+                >
+                    {toasts?.map((toast, index) => {
+                        if(index > MAX_TOASTS - 1) { return; }
+                        return (
+                            <Toast
+                                key={toast.id}
+                                id={toast.id}
+                                title={toast?.title}
+                                description={toast?.description}
+                                action={toast.action}
+                                close={() => closeToast(toast.id)}
+                                duration={toast.duration}
+                                variant={toast.variant}
+                            />
+                        )})
+                    }
+                </section>
+            </>
         </ToastContext.Provider>
     )
 }
 
-const Toast = ({ message, close }: { message: string, close: () => void }) => {
+const Toast = ({ id, title, description, action, duration, variant, close, ...props }: ToastProps) => {
+
+    const composeEvents = () => {
+        if(action?.callback !== undefined) {
+            action?.callback();
+        }
+        close();
+    }
+
     return(
-        <div className="toast">
-            <p>{message}</p>
-            <button onClick={close}>{`\u274c`}</button>
+        <div className={`${css.toast} ${css[variant]}`} {...props}>
+            <div className={css.toast_content}>
+                {title &&
+                    <p className={css.title}>
+                        {title}
+                    </p>
+                }
+                {description &&
+                    <p className={css.description}>
+                        {description}
+                    </p>
+                }
+            </div>
+            {action &&
+                <div className={css.action_container}>
+                    <button
+                        className={`${css.action_button}`}
+                        {...props}
+                        onClick={() => composeEvents()}
+                    >
+                        {action?.label}
+                    </button>
+                </div>
+            }
+            <button
+                className={css.close_button}
+                aria-name="close"
+                onClick={() => close()}
+            >
+                <svg
+                    className={css.close_icon}
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="17"
+                    width="17"
+                    viewBox="0 -960 960 960"
+                >
+                    <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/>
+                </svg>
+            </button>
         </div>
     )
 }
---
+++ src/ui/components/toast/toast.types.ts
@@ -0,0 +1,55 @@
+import * as React from "react";
+
+type ToastPosition = "top_left" | "top_right" | "bottom_left" | "bottom_right";
+
+type ToastVariant = "primary" | "secondary" | "success" | "destructive" | "warning" ;
+
+type ToastAction = {
+    label: string;
+    callback: () => {};
+}
+
+interface ToastContextProps {
+    create: (content: any) => void;
+    close: (id: number) => void;
+}
+
+interface ToastProviderProps {
+    children: any;
+    position?: ToastPosition;
+    className?: string;
+}
+
+interface ToastDataProps {
+    id: number;
+    content: any;
+}
+
+interface ToastCreateProps {
+    id: number;
+    title?: React.ReactNode;
+    description?: React.ReactNode;
+    action?: ToastAction;
+    duration?: number;
+    variant: ToastVariant;
+}
+
+interface ToastProps {
+    id: number;
+    title?: React.ReactNode;
+    description?: React.ReactNode;
+    action?: ToastAction;
+    duration?: number;
+    variant: ToastVariant;
+    close: (id?: number) => void;
+}
+
+export type {
+    ToastContextProps,
+    ToastDataProps,
+    ToastProviderProps,
+    ToastAction,
+    ToastProps,
+    ToastPosition,
+    ToastCreateProps
+}
