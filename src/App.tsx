49cd5ce561ccc3cb544f6d96cfff9c654f413361
Etienne <34932238+EtienneCClarke@users.noreply.github.com>
2024-03-26 23:46:04 +0000

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,21 @@ import AccordionExample from "./ui/examples/accordion-example";
 import AlertExample from "./ui/examples/alert-example";
 import ButtonExample from "./ui/examples/button-example";
 import BadgeExample from "./ui/examples/badge-example";
-import { ToastProvider, useToast } from "./ui/components/toast";
-import "./app.css";
 import ToastExample from "./ui/examples/toast-example";
+import AlertDialogExample from "./ui/examples/alert-dialog-example";
+import CalendarExample from "./ui/examples/calendar-example";
+import "./app.css";
 
 function App() {
-
-  const toast = useToast();
 
   return (
     <>
       <AccordionExample />
       <AlertExample />
+      <AlertDialogExample />
       <BadgeExample />
       <ButtonExample />
+      <CalendarExample />
       <ToastExample />
     </>
   );
