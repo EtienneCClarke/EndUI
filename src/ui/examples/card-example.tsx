5e67d63628a2c05ba43cfcfc02b644ded73570df
Etienne <34932238+EtienneCClarke@users.noreply.github.com>
2024-04-04 20:40:53 +0100

    Changed select in card content to use Select from EndUI

--- src/ui/examples/card-example.tsx
+++ src/ui/examples/card-example.tsx
@@ -1,5 +1,6 @@
 import { Button } from "../components/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "../components/card";
+import { Select, SelectTrigger, SelectContent, SelectItem } from "../components/select";
 import { Input } from "../components/input";
 
 export default function CardExample() {
@@ -16,12 +17,15 @@ export default function CardExample() {
                     <label htmlFor="name">Name</label>
                     <Input placeholder="Name of your project" />
                     <label htmlFor="framework">Framework</label>
-                    <select>
-                        <option selected>Next.js</option>
-                        <option>SveltKit</option>
-                        <option>Astro</option>
-                        <option>Nuxt.js</option>
-                    </select>
+                    <Select defaultValue={"next"} defaultLabel={"Next.js"}>
+                        <SelectTrigger></SelectTrigger>
+                        <SelectContent>
+                            <SelectItem value="Next.js">Next.js</SelectItem>
+                            <SelectItem value="SveltKit">SveltKit</SelectItem>
+                            <SelectItem value="Astro">Astro</SelectItem>
+                            <SelectItem value="Nuxt.js">Nuxt.js</SelectItem>
+                        </SelectContent>
+                    </Select>
                 </CardContent>
                 <CardFooter style={{ display: 'flex', justifyContent: 'space-between'}}>
                     <Button variant='outline'>Cancel</Button>
